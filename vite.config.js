34a0b5303e1f90b486ac1571d52de2d348285811
Elton Costa <elton.du@outlook.com>
2026-03-20 15:41:50 -0300

    ajusta base para deploy no vercel

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/calendario_de_eventos/", // 👈 ESSENCIAL
+  plugins: [react(), tailwindcss()],  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
